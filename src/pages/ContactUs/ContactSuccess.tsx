407ce7fe33d384fa3817136f0a347bd29c6af553
UGamerZ <gasterdreemyr@gmail.com>
2025-05-07 15:53:47 +0800

    added success server response rendering

--- src/pages/ContactUs/ContactSuccess.tsx
+++ src/pages/ContactUs/ContactSuccess.tsx
@@ -1,14 +1,16 @@
 import { FormWrapper } from "../../styles/style.ts";
 import { Typography } from "@mui/joy";
 import { sendContactAction } from "./sendContactActions.ts";
+import { useState } from "react";
 
 const ContactSuccess = () => {
+  const [response, setResponse] = useState();
+  sendContactAction().then((resp) => setResponse(resp));
+
   return (
     <FormWrapper>
       <Typography level="h1">Message generated on the server</Typography>
-      {sendContactAction().then((response) => (
-        <Typography>{response}</Typography>
-      ))}
+      {response && <Typography>{response}</Typography>}
     </FormWrapper>
   );
 };
