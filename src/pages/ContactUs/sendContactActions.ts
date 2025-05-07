a30a00609f872258f697cdabaea837e6e442b615
UGamerZ <gasterdreemyr@gmail.com>
2025-05-07 19:01:21 +0800

    function request edit

--- src/pages/ContactUs/sendContactActions.ts
+++ src/pages/ContactUs/sendContactActions.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const sendContactAction = async () => {
   const response = await axios.post(
-    "https://cadexhir.netlify.app/.netlify/functions/contact",
+    "https://cadexhiring.netlify.app/.netlify/functions/contact",
     document.querySelector("#contact-form"),
     {
       headers: {
