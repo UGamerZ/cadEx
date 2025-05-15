0e04decf1a3eb4ff6145565db5366d31b1e6f10b
Vladislav <70677188+UGamerZ@users.noreply.github.com>
2025-05-15 10:49:15 +0800

    Memo optimization

--- src/pages/ContactUs/ContactSuccess.tsx
+++ src/pages/ContactUs/ContactSuccess.tsx
@@ -1,13 +1,14 @@
 import { FormWrapper } from "../../styles/style.ts";
 import { Typography } from "@mui/joy";
+import { memo } from 'react';
 
-const ContactSuccess = ({ response }: { response: string | undefined }) => {
+const ContactSuccess = memo(({ response }: { response: string | undefined }) => {
   return (
     <FormWrapper>
       <Typography level="h1">Message generated on the server</Typography>
       {response && <Typography>{response}</Typography>}
     </FormWrapper>
   );
-};
+});
 
 export default ContactSuccess;
